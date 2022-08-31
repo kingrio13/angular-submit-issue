f528bca6257ca584896a797a7040c81a2292e5da
kingrio13 <kingrio.miranda@gmail.com>
2022-08-31 11:50:52 +0800

    submit issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ClarityModule } from "@clr/angular";
 import { AppComponent } from './app.component';
+import { IssueListComponent } from './issue-list/issue-list.component';
+import { ReactiveFormsModule } from '@angular/forms'; 
+import { IssueReportComponent } from './issue-report/issue-report.component';
+import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IssueListComponent,
+    IssueReportComponent,
+    ConfirmDialogComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ClarityModule,
+    ReactiveFormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/issue-list/issue-list.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { IssuesService } from '../issues.service';
+import { Issue } from '../issue';
+
+
+
+@Component({
+  selector: 'app-issue-list',
+  templateUrl: './issue-list.component.html',
+  styleUrls: ['./issue-list.component.scss']
+})
+export class IssueListComponent implements OnInit {
+  issues: Issue[] = [];
+  showReportIssue = false;
+  selectedIssue: Issue | null = null;
+
+  constructor(private issueService: IssuesService) { }
+
+
+  private getIssues() {
+    this.issues = this.issueService.getPendingIssues();
+  }
+
+  ngOnInit(): void {
+    this.getIssues();
+  }
+
+  onCloseReport() {
+    this.showReportIssue = false;
+    this.getIssues();
+  }
+
+  onConfirm(confirmed: boolean) {
+    if (confirmed && this.selectedIssue) {
+      this.issueService.completeIssue(this.selectedIssue);
+      this.getIssues();
+    }
+    this.selectedIssue = null;
+
+  }
+
+  
+
+}
